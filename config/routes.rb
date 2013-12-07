c51cb15d77e978334d11def25bd899bc6209dbbf
Matheus Bodo <mcbodo@gmail.com>
2013-12-06 23:59:38 -0200

    creating artists model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RocksmithCustomLibrary::Application.routes.draw do
+  resources :artists
+
   root :to => "home#index"
   devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206231809) do
+ActiveRecord::Schema.define(version: 20131207010214) do
+
+  create_table "artists", force: true do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
